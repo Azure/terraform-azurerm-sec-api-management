41cb6f413a55d8463ec89751933388f64da3d120
Luke Devonshire <luke@MacBook-Pro.local>
2020-06-07 18:02:22 +0100

    Changed module to output full apim object

--- outputs.tf
+++ outputs.tf
@@ -1,18 +1,3 @@
-output "apim_gateway_url" {
-  value = azurerm_api_management.apim.gateway_url
-}
-output "apim_name" {
-  value = azurerm_api_management.apim.name
-}
-output "apim_management_api_url" {
-  value = azurerm_api_management.apim.management_api_url
-}
-output "apim_portal_url" {
-  value = azurerm_api_management.apim.portal_url
-}
-output "apim_public_ips" {
-  value = azurerm_api_management.apim.public_ip_addresses
-}
-output "apim_identity" {
-  value = azurerm_api_management.apim.identity
+output "api_management" {
+  value = azurerm_api_management.apim
 }
